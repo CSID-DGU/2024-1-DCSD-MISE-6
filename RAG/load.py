031041158f9ae728f6485abc7fc562d65d92f893
seonggeuns <100848654+seonggeuns@users.noreply.github.com>
2024-05-07 23:23:36 +0900

    Delete RAG directory

--- RAG/load.py
+++
@@ -1,3 +0,0 @@
-from langchain_community.document_loaders import CSVLoader
-loader = CSVLoader(file_path='/content/drive/MyDrive/데이터사이언스캡스톤/판례데이터셋/total_dataset.csv') # /home/mise6/mise_code/model/LLM/final_clean_legal2.csv
-document = loader.load()
