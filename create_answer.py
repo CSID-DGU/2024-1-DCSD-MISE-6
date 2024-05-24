df778ef5af98fa48d8f72d1ae1a1c404fe34ce37
seonggeuns <ska07072@gmail.com>
2024-05-24 10:15:25 +0900

    fine-tuning model id 추가

--- create_answer.py
+++ create_answer.py
@@ -7,7 +7,7 @@ def create_output(text):
     client = OpenAI(api_key='key')
 
     completion = client.chat.completions.create(
-    model="fine-tuning model",
+    model="ft:gpt-3.5-turbo-1106:personal:final5:9S6zzH2V", # model id 추가
     messages=[
         {"role": "system", "content": "You are a legal expert who must explain as quickly and concisely as possible."},
         {"role": "user", "content": text}
